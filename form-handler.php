f842557a8266a6a7c1bf02b28e95969c9095eed0
root <root@web-everydance.eclypsedev.fr>
2022-04-18 13:52:34 +0000

    update email

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'no-reply@skygames.fr';
+$email_from = 'no-reply@everydance.fr';
 
 $email_subject = '$subject';
 
